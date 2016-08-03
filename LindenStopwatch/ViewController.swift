e8e99fd181559496590ccb98c4174c2d5560d632
thefreshduke <scottyshaw1@gmail.com>
2016-08-03 11:36:38 -0700

    Created alternateButtons()

--- LindenStopwatch/ViewController.swift
+++ LindenStopwatch/ViewController.swift
@@ -10,26 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     
     @IBAction func startTime(sender: AnyObject) {
         timeLabel.text = "start"
+        alternateButtons()
     }
     
     @IBAction func stopTime(sender: AnyObject) {
         timeLabel.text = "stop"
+        alternateButtons()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.hidden = true
+        stopButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func alternateButtons() {
+        startButton.hidden = !startButton.hidden
+        startButton.enabled = !startButton.enabled
+        stopButton.hidden = !stopButton.hidden
+        stopButton.enabled = !stopButton.enabled
+    }
 }
 
